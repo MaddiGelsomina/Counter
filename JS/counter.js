4628ddfc987202a53f9231b8c569f6cabb4c7426
MaddiGelsomina <gelsominamaddi@gmail.com>
2022-08-11 09:48:57 +0200

    Modifica file counter.js.Aggiunti i button

--- JS/counter.js
+++ JS/counter.js
@@ -1,25 +1,54 @@
-let counter = document.querySelector('.counter');
-let counterPlus = document.querySelector('.counter-plus');
-let counterMinus = document.querySelector('.counter-minus');
+let container = document.createElement("div");
+container.setAttribute("class","container");
+
+let pCounter = document.createElement("p");
+pCounter.setAttribute("id","counter");
+pCounter.setAttribute("class","counter");
+
+let buttonsContainer = document.createElement("div");
+buttonsContainer.setAttribute("class","buttons-container");
+
+document.body.appendChild(container);
+container.appendChild(pCounter);
+container.appendChild(buttonsContainer);
+
+let btnPlus = document.createElement("button");
+btnPlus.innerHTML= "+";
+btnPlus.setAttribute("id","increment-btn");
+btnPlus.setAttribute("class","counter-plus button");
+
+let btnMinus = document.createElement("button");
+btnMinus.innerHTML= "-";
+btnMinus.setAttribute("id","decrement-btn");
+btnMinus.setAttribute("class", "counter-minus button");
+
+buttonsContainer.appendChild(btnPlus);
+buttonsContainer.appendChild(btnMinus);
+
+let counter = document.getElementById("counter");
+
+let buttonPlus = document.getElementById("increment-btn");
+let buttonMinus = document.getElementById("decrement-btn");
 
 let count = 0;
 
-changeCounter();
+counter.innerHTML = count;
 
-counterPlus.addEventListener("click", () => {
+buttonPlus.addEventListener("click", increment);
+buttonMinus.addEventListener("click", decrement);
+
+function increment(){
     count++;
-    changeCounter();
-});
-
-counterMinus.addEventListener("click", () => {
-    if(count > 0){
-        count--;
-    }else if(count = 0){
-        count = 0;
-    }
-    changeCounter();
-});
-
-function changeCounter(){
-    counter.innerHTML = count;
+    counter.innerHTML = count
 };
+
+function decrement(){
+    if(count<=0){
+        count == 0;
+    }else{
+    count--;
+    }
+    counter.innerHTML = count
+};
+
+
